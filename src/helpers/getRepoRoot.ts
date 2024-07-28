d246c28f70f5cd2ac40110b2d376ad0bac59384d
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2024-07-27 21:47:50 -0400

    fix: refactor fs import

--- src/helpers/getRepoRoot.ts
+++ src/helpers/getRepoRoot.ts
@@ -1,10 +1,11 @@
 'use strict';
-import * as fs from 'node:fs';
+import { promises as fsPromises, readFile, stat, readdir } from 'node:fs';
 import git from 'isomorphic-git';
 
 export async function getRepoRoot(): Promise<string> {
+  const fs = { promises: fsPromises, readFile, stat, readdir };
   const repoRoot = await git.findRoot({
-    fs: { ...fs, promises: fs.promises },
+    fs,
     filepath: process.cwd(),
   });
   return repoRoot;
